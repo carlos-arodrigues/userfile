9e9930aa1a951519b8dafcd09df4cc20e6202533
Carlos Antonio Rodrigues <carlos.arodrigues@gmail.com>
2017-08-23 18:31:43 -0300

    Improving

--- src/main/java/com/sargentdisc/domain/model/userfile/UserFile.java
+++ src/main/java/com/sargentdisc/domain/model/userfile/UserFile.java
@@ -11,7 +11,10 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.Validate;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
 
 @Entity
 @Table(name = "USERFILE")
@@ -21,24 +24,25 @@ public class UserFile implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "id")
-    private Long id;
-	
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id")
+	private Long id;
+
 	@Column(name = "name", length = 50, nullable = false)
 	private String name;
-	
+
 	@Column(name = "location", nullable = false)
 	private String location;
-	
-	@Column(name = "text")
+
+	@Column(name = "text", length = 510)
 	private String text;
-	
-	public UserFile(){}
-	
+
+	public UserFile() {
+	}
+
 	public UserFile(String name, String location, String text) {
-		Validate.validState(name != null && !name.contains(" ") && name.length() <=50);
-		Validate.validState(location != null && location.length() <=100);
+		Validate.validState(name != null && !name.contains(" ") && name.length() <= 50);
+		Validate.validState(location != null && location.length() <= 100);
 		this.name = name;
 		this.location = location;
 		this.text = text;
@@ -60,40 +64,86 @@ public class UserFile implements Serializable {
 		return text;
 	}
 
+	public void removeCommasWithSpace() {
+		if(StringUtils.isEmpty(this.text)){
+			return;
+		}
+		StringBuffer sb = new StringBuffer();
+		char[] array = text.toCharArray();
+		for(int i=0 ; i < array.length ; i++){
+			if(array[i] == ',' && array[i+1] == ' '){
+				i++;
+			}
+			sb.append(array[i]);
+		}
+		this.text = sb.toString();
+		
+	}
+	
+	@Override
+	public int hashCode() {
+		return new HashCodeBuilder()
+				.append(this.id)
+				.append(this.name)
+				.append(this.location)
+				.append(this.text)
+				.build();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (!(obj instanceof UserFile)) {
+			return false;
+		}
+		UserFile other = (UserFile) obj;
+		return new EqualsBuilder()
+				.append(this.id, other.getId())
+				.append(this.name, other.getName())
+				.append(this.location, other.getLocation())
+				.append(this.text, other.getText())
+				.isEquals();
+	}	
+
 	public static WithMandatories named(String name) {
 		return new BuilderUserFile().withName(name);
 	}
-	
-	public interface WithMandatories{
+
+	public interface WithMandatories {
 		public BuilderUserFile withLocation(String location);
+
 		public BuilderUserFile withName(String name);
 	}
-	
-	public static class BuilderUserFile implements WithMandatories{
+
+	public static class BuilderUserFile implements WithMandatories {
 		private String name;
 		private String location;
 		private String text;
-		
-		public BuilderUserFile withName(String name){
+
+		public BuilderUserFile withName(String name) {
 			this.name = name;
 			return this;
 		}
-		
+
 		public BuilderUserFile withLocation(String location) {
 			this.location = location;
 			return this;
 		}
-		
+
 		public BuilderUserFile withText(String text) {
 			this.text = text;
 			return this;
 		}
-		
+
 		public UserFile build() {
 			return new UserFile(name, location, text);
 		}
 
 	}
 
-	
 }
--- src/test/java/com/sargentdisc/domain/model/UserFile/UserFileUnitTest.java
+++ src/test/java/com/sargentdisc/domain/model/UserFile/UserFileUnitTest.java
@@ -4,9 +4,14 @@ import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 import org.junit.Assert;
 import org.junit.Test;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sargentdisc.domain.model.userfile.UserFile;
 
 
@@ -64,6 +69,39 @@ public class UserFileUnitTest{
 	public void should_build_userfile_with_validations_failing_location_with_big_length() throws Exception {
 		UserFile.named("Thomas").withLocation("Londonnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn"
 				+ "nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn").build();
+	}	
+	
+	@Test
+	public void should_do_nothing_when_removing_commas() throws Exception {
+		UserFile file = UserFile.named("Thomas").withLocation("London").build();
+		file.removeCommasWithSpace();
+		Assert.assertNull(file.getText());
+	}		
+	
+	@Test
+	public void should_remove_commas_with_spaces_from_text() throws Exception {
+		String sbJSON = loadFile();
+		UserFile userFile = new ObjectMapper().readValue(sbJSON, UserFile.class);
+		String textBefore = userFile.getText();
+		userFile.removeCommasWithSpace();
+		Assert.assertFalse(textBefore.equals(userFile.getText()));
+		Assert.assertFalse(userFile.getText().contains(", "));
+	}
+
+	private String loadFile() {
+		File file = new File(this.getClass().getClassLoader().getResource("DocumentTest.txt").getFile());
+		StringBuffer sbJSON = new StringBuffer();
+		try (Scanner scanner = new Scanner(file)) {
+			while (scanner.hasNextLine()) {
+				String line = scanner.nextLine();
+				sbJSON.append(line).append("\n");
+			}
+			scanner.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return sbJSON.toString();
 	}	
 
 }
